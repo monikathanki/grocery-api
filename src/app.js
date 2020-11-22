f9ad17ee38c789f3376e4b3501d22261749136a4
Monikathanki <thanki.monika2@gmail.com>
2020-11-21 19:55:08 -0700

    second commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ let morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 let { NODE_ENV } = require('./config')
+let errorHandler = require('./error-handler')
 
 const app = express()
 
@@ -14,20 +15,13 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
+
+
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = {error: {message: "server error"}}
-    } else {
-        console.error(error)
-        response = {message: error.message, error}
-    }
-    res.status(500).json(response)
-})
+app.use(errorHandler)
 
 app.use(cors())
 
