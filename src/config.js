31c71a77693acb532c0e429bbb465a79bf03470b
monika <thanki.monika@gmail.com>
2021-03-24 18:20:35 -0700

    heroku

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = {
   API_TOKEN: process.env.API_TOKEN ,
   JWT_SECRET: process.env.JWT_SECRET || 'grocery-list-api-jwt',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL|| TEST_DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL|| this.TEST_DATABASE_URL,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
 
 }
